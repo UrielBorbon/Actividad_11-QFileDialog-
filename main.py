e6b76d24d082310f9772179bcf5aa9d4ce75a07e
Uriel Borbon <alfredo.medina4934@alumnos.udg.mx>
2020-10-22 18:01:56 -0500

    Actividad 9 Interfaz con Funciones y el TextPlainEdit

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-from particula import Particula
-
-class Particle:
-    def __init__(self):
-        self.__particles = []
-
-    def agregar_final(self, particula:Particula):
-        self.__particles.append(particula)
-
-    def agregar_inicio(self, particula:Particula):
-        self.__particles.insert(0, particula)
-
-    def mostrar(self):
-        for particula in self.__particles:
-            print(particula)
-
-#part = Particle()
-#pa1 = Particula(1,1,1,1,1,1,1,1,1,0)
-#pa2 = Particula(1,100,200,200,1,4,2,2,2,0)
-#part.agregar_inicio(pa2)
-#part.agregar_final(pa1)
-#part.mostrar()
+from PySide2.QtWidgets import QPushButton, QApplication
+import sys
+from mainwindow import MainWindow
+# Aplicación de Qt
+app = QApplication()
+window = MainWindow()
+window.show()
+# Qt loop
+sys.exit(app.exec_())
